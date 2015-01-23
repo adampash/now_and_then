23dc089f22a088248edfe2d66cf8c0ffd60bf7a8
Dan Tello <dan.tello@gmail.com>
2015-01-23 18:50:14 -0500

    Run karma tests as part of production task

--- gulp/tasks/karma.js
+++ gulp/tasks/karma.js
@@ -1,25 +1,13 @@
 var gulp = require('gulp');
-var config       = require('../config').karma;
-var karma = require('karma').server;
+var karma = require('karma');
 
-console.log(config.conf);
+var karmaTask = function(done) {
+  karma.server.start({
+    configFile: process.cwd() + '/karma.conf.js',
+    singleRun: true
+  }, done)
+};
 
-/**
- * Run test once and exit
- */
-gulp.task('test', function (done) {
-  karma.start({
-    // configFile: __dirname + '/karma.conf.js',
-    configFile: config.conf
-    // singleRun: true
-  }, done);
-});
+gulp.task('karma', karmaTask);
 
-/**
- * Watch for file changes and re-run tests on each change
- */
-gulp.task('tdd', function (done) {
-  karma.start({
-    configFile: config.conf
-  }, done);
-});
+module.exports = karmaTask;
